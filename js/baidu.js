42927f4d50bc0573efef2e6a2d9590415ba121ac
l1nker4 <l1nker4@outlook.com>
2025-01-12 20:38:09 +0800

    add baidu statistics file

---
+++ js/baidu.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?612bbac3c11e86af5c472b8381f09fa7";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
